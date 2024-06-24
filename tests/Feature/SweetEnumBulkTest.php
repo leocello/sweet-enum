a004b6b4ab78c53862c450b6a8d4af9bf968af7c
Leo Oberle <leocello@gmail.com>
2024-06-25 02:20:11 +1200

    Separate single cases to bulk tests for better organisation

---
+++ tests/Feature/SweetEnumBulkTest.php
@@ -0,0 +1,106 @@
+<?php
+
+use Leocello\SweetEnum\Tests\Enums\Color;
+
+describe('SweetEnum bulk', function () {
+    it ('is possible to run callback in each active option and collect results', function () {
+        $string = '';
+
+        Color::foreach(callback: function (Color $color) use (&$string) {
+            if (strlen($string) > 0) {
+                $string .= ', ';
+            }
+
+            $string .= $color->id();
+        });
+
+        expect($string)->toBeString()
+            ->and($string)->toContain('white', 'black', 'red', 'green', 'blue')
+            ->and($string)->not()->toContain('yellow')
+        ;
+    });
+
+    it ('is possible to run callback in each option (include inactive) and collect results', function () {
+        $string = '';
+
+        Color::foreach(callback: function (Color $color) use (&$string) {
+            if (strlen($string) > 0) {
+                $string .= ', ';
+            }
+
+            $string .= $color->id();
+        }, onlyActives: false);
+
+        expect($string)->toBeString()
+            ->and($string)->toContain('white', 'black', 'red', 'green', 'blue')
+            ->and($string)->toContain('yellow')
+        ;
+    });
+
+    it ('is possible to map based on callback in each active option and collect results', function () {
+        $results = Color::map(callback: function (Color $color) {
+            return 'it\'s a ' . strtolower($color->title());
+        });
+
+        expect($results)->toBeArray()
+            ->and($results)->toMatchArray([
+                'blue' => 'it\'s a blue color',
+                'green' => 'it\'s a green color',
+            ])
+            ->and($results)->not()->toHaveKeys([
+                'yellow',
+            ])
+        ;
+    });
+
+    it ('is possible to map based on callback in each option (include inactive) and collect results', function () {
+        $results = Color::map(callback: function (Color $color) {
+            return 'it\'s a ' . strtolower($color->title());
+        }, onlyActives: false);
+
+        expect($results)->toBeArray()
+            ->and($results)->toMatchArray([
+                'blue' => 'it\'s a blue color',
+                'green' => 'it\'s a green color',
+                'yellow' => 'it\'s a yellow color',
+            ])
+        ;
+    });
+
+    it('is possible to return all active cases info as array', function () {
+        $info = Color::getCasesInfo(fields: ['id', 'title', 'hex', 'rgb']);
+
+        expect($info)->toBeArray()
+            ->and($info)->toHaveKeys([
+                'blue',
+                'green',
+                'white',
+            ])
+            ->and($info)->not()->toHaveKeys([
+                'yellow',
+            ])
+            ->and($info['blue'])->toMatchArray([
+                'id' => 'blue',
+                'title' => 'Blue color',
+                'hex' => '#0000FF',
+                'rgb' => [0, 0, 255],
+            ])
+        ;
+    });
+
+    it('is possible to return all cases (including inactive) info as array', function () {
+        $info = Color::getCasesInfo(fields: ['id', 'title', 'hex', 'rgb'], onlyActives: false);
+
+        expect($info)->toBeArray()
+            ->and($info)->toHaveKeys([
+                'yellow',
+            ])
+            ->and($info['yellow'])->toMatchArray([
+                'id' => 'yellow',
+                'title' => 'Yellow color',
+                'hex' => '#FFFF00',
+                'rgb' => [255, 255, 0],
+            ])
+        ;
+    });
+});
---
+++ tests/Feature/SweetEnumSingleCaseTest.php
@@ -0,0 +1,94 @@
+<?php
+
+use Leocello\SweetEnum\Tests\Enums\Color;
+
+describe('SweetEnum single case', function () {
+    it ('can be used as normal enums', function () {
+        $red = Color::Red;
+        $green = Color::Green;
+
+        expect($red->name)->toBe('Red')
+            ->and($red->value)->toBe('red')
+            ->and($green->name)->toBe('Green')
+            ->and($green->value)->toBe('green')
+        ;
+    });
+
+    it ('can access properties as methods', function () {
+        $blue = Color::Blue;
+        $yellow = Color::Yellow;
+
+        expect($blue->id())->toBe('blue')
+            ->and($blue->title())->toBe('Blue color')
+            ->and($blue->isOn())->toBeTrue()
+            ->and($blue->hex())->toBe('#0000FF')
+            ->and($yellow->isOn())->toBeFalse()
+        ;
+    });
+
+    it ('can access computed properties as methods', function () {
+        $blue = Color::Blue;
+        $yellow = Color::Yellow;
+
+        expect($blue->id())->toBe('blue')
+            ->and($blue->rgb())->toMatchArray([0, 0, 255])
+            ->and($yellow->id())->toBe('yellow')
+            ->and($yellow->rgb())->toMatchArray([255, 255, 0])
+        ;
+    });
+
+    it ('uses enum name as title if not explicit', function () {
+        $black = Color::Black;
+        $white = Color::White;
+
+        expect($white->title())->toBe('White')
+            ->and($black->name())->toBe('Black')
+        ;
+    });
+
+    it ('can return all its basic values as an array', function () {
+        expect(Color::Blue->toArray())->toBe([
+            'id' => 'blue',
+            'title' => 'Blue color',
+        ]);
+    });
+
+    it ('can return all its original values as an array', function () {
+        expect(Color::Blue->toArray(Color::FIELDS_ORIGINAL))->toBe([
+            'value' => 'blue',
+            'name' => 'Blue',
+        ]);
+    });
+
+    it ('can return all its basic values with the status as an array', function () {
+        expect(Color::Blue->toArray(Color::FIELDS_SWEET_WITH_STATUS))->toBe([
+            'isOn' => true,
+            'id' => 'blue',
+            'title' => 'Blue color',
+        ]);
+    });
+
+    it ('can return all its values (including custom) with the status as an array', function () {
+        expect(Color::Blue->toArray(Color::FIELDS_SWEET_FULL))->toBe([
+            'isOn' => true,
+            'value' => 'blue',
+            'id' => 'blue',
+            'name' => 'Blue',
+            'title' => 'Blue color',
+            'hex' => '#0000FF',
+            'rgb' => [0, 0, 255],
+        ]);
+    });
+
+    it ('can return customised values as an array', function () {
+        expect(Color::Blue->toArray(['id', 'hex', 'rgb']))->toBe([
+            'id' => 'blue',
+            'hex' => '#0000FF',
+            'rgb' => [0, 0, 255],
+        ]);
+    });
+
+    test ('at least one field is required if trying to get customised values as an array', function () {
+        Color::Blue->toArray([]);
+    })->throws(InvalidArgumentException::class);
+});
