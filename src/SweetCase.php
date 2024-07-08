812a84adcf2fb3bf47868b09adbb91f05853e621
Leonardo Oberle <leocello@gmail.com>
2024-07-08 23:29:45 +1200

    * Update `SweetCase` to use named arguments instead of `custom` on constructor (also reorder arguments) (#3)

--- src/SweetCase.php
+++ src/SweetCase.php
@@ -5,12 +5,23 @@ namespace Leocello\SweetEnum;
 #[\Attribute]
 class SweetCase
 {
+    public readonly ?string $caseClass;
+
+    public readonly ?string $title;
+
+    public readonly bool $isOn;
+
+    public readonly array $custom;
+
     public function __construct(
-        public readonly ?string $title = null,
-        public readonly bool $isOn = true,
-        public readonly array $custom = [],
-        public readonly ?string $caseClass = null,
+        ?string $caseClass = null,
+        ?string $title = null,
+        bool $isOn = true,
+        ...$custom
     ) {
-        ///
+        $this->caseClass = $caseClass;
+        $this->title = $title;
+        $this->isOn = $isOn;
+        $this->custom = $custom;
     }
 }
